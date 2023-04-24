3dde5b52a28eab0ccd0b46400e7a4fe3e0679b01
c21333116 <123654910+c21333116@users.noreply.github.com>
2023-04-24 20:30:00 +0100

    killig

---
+++ tune.java
@@ -0,0 +1,35 @@
+class Nematode{
+
+	private int m;
+	private int f;
+	private int n;
+	private in
+
+	public Nematode(int m, int f, int n, Tablerow row){
+
+		this.m = m;
+		this.f = f;
+		this.n = n;
+		this.row = row;
+		
+	}
+
+	public void 
+
+	public class String toString
+	{
+
+	}
+
+}
+
+class NematodeVisualizer{
+
+	public void loadNematodes(){
+
+	}
+
+
+
+
+}
